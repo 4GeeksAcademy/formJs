5f194ec103ecc9146e60d45a321cd7cd210c94f1
MarcelaZenteno93 <126695026+MarcelaZenteno93@users.noreply.github.com>
2023-11-09 02:53:42 +0000

    Corregido

--- src/app.js
+++ src/app.js
@@ -5,53 +5,83 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//write your code here
-
-// Agrega este código en el mismo archivo
 document.addEventListener("DOMContentLoaded", function() {
   document
     .getElementById("submitBtn")
     .addEventListener("click", function(event) {
       event.preventDefault(); // Detiene el envío del formulario
 
-      // Realiza la validación de los campos aquí
-      // Por ejemplo:
-      const cardNumber = document.getElementById("Card#").value;
-      const cvc = document.getElementById("CVC#").value;
-      const amount = document.getElementById("amount").value;
-
       // Lógica de validación
       let isValid = true;
-      if (!cardNumber || cardNumber.length !== 16) {
-        // Card # no válido
-        isValid = false;
-        document.getElementById("Card#").style.backgroundColor = "lightcoral";
-      } else {
-        document.getElementById("Card#").style.backgroundColor = "";
-      }
+      const errorMessages = [];
 
-      if (!cvc || cvc.length !== 4) {
-        // CVC # no válido
-        isValid = false;
-        document.getElementById("CVC#").style.backgroundColor = "lightcoral";
-      } else {
-        document.getElementById("CVC#").style.backgroundColor = "";
-      }
+      const fieldsToValidate = [
+        { id: "Card#", validation: validateCardNumber },
+        { id: "CVC#", validation: validateCVC },
+        { id: "amount", validation: validateAmount },
+        { id: "FirstName#", validation: validateFirstName },
+        { id: "LastName#", validation: validateLastName },
+        { id: "city", validation: validateCity },
+        { id: "postalcode", validation: validatePostalCode }
+      ];
 
-      if (!amount || isNaN(amount)) {
-        // Amount no válido
-        isValid = false;
-        document.getElementById("amount").style.backgroundColor = "lightcoral";
-      } else {
-        document.getElementById("amount").style.backgroundColor = "";
-      }
+      fieldsToValidate.forEach(field => {
+        const element = document.getElementById(field.id);
+        const value = element.value;
+        if (!field.validation(value)) {
+          isValid = false;
+          element.style.backgroundColor = "lightcoral";
+          errorMessages.push(`Invalid ${field.id}`);
+        } else {
+          element.style.backgroundColor = "white";
+        }
+      });
 
-      // Muestra u oculta el mensaje de error
-      const errorMessage = document.getElementById("dangerous");
       if (!isValid) {
-        errorMessage.style.display = "block"; // Muestra el mensaje de error
-      } else {
-        errorMessage.style.display = "none"; // Oculta el mensaje de error
+        // Mostrar errores en la parte superior del formulario
+        const errorContainer = document.getElementById("dangerous");
+        if (errorMessages.length > 0) {
+          errorContainer.innerHTML = errorMessages.join("<br>");
+          errorContainer.style.display = "block";
+        } else {
+          errorContainer.style.display = "none";
+        }
       }
     });
 });
+
+// Funciones de validación
+function validateCardNumber(value) {
+  // Lógica de validación para el número de tarjeta
+  return /^\d{16}$/.test(value);
+}
+
+function validateCVC(value) {
+  // Lógica de validación para el CVC
+  return /^[0-9]{3,4}$/.test(value);
+}
+
+function validateAmount(value) {
+  // Lógica de validación para el monto
+  return !isNaN(value) && parseFloat(value) > 0;
+}
+
+function validateFirstName(value) {
+  // Lógica de validación para el nombre
+  return /^[A-Za-z]+$/.test(value);
+}
+
+function validateLastName(value) {
+  // Lógica de validación para el apellido
+  return /^[A-Za-z]+$/.test(value);
+}
+
+function validateCity(value) {
+  // Lógica de validación para la ciudad
+  return /^[A-Za-z\s]+$/.test(value);
+}
+
+function validatePostalCode(value) {
+  // Lógica de validación para el código postal
+  return /^\d{5}$/.test(value);
+}
